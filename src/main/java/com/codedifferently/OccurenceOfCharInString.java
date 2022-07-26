14567534c0d17f5b94e9f19377cf81f37bbf033a
Tory Doby <torydoby43@gmail.com>
2022-07-26 09:35:10 -0400

    update

---
+++ src/main/java/com/codedifferently/OccurenceOfCharInString.java
@@ -0,0 +1,21 @@
+package com.codedifferently;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OccurenceOfCharInString {
+    public static <Count> void characterCount(String inputString) {
+        Map<Character, Count> charCountMap = new HashMap<Character,Count>();
+        char[] strArray = inputString.toCharArray();
+        for (int i = 0; i<strArray.length;i++){
+            strArray.charAt(i)
+        }
+            if (charCountMap.containsKey(c)) {
+                charCountMap.put(), charCountMap.get(c) + 1;);
+
+            }
+            else {
+                return
+            }
+    }
+}
--- src/main/java/com/codedifferently/Solution.java
+++ src/main/java/com/codedifferently/Solution.java
@@ -2,6 +2,16 @@ package com.codedifferently;
 
 public class Solution {
     public String howManyLettersDoYouSee(String input){
-        return null;
+        int count = 0;
+        for (int i = 0;i < input.length();i++){
+            for (int j = 1; j<input.length();j++){
+                if (input.charAt(i) == input.charAt(j)){
+                    count++;
+                }
+            }
+        }
+        return ;
     }
 }
+
+
